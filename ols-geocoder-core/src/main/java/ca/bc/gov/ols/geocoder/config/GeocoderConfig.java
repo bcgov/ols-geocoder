a6b577d6bec049e5b6a3911c439aac2c509e14cf
Dave Kelsey <David.Kelsey@gov.bc.ca>
2021-08-05 09:34:36 -0700

    update version to 4.1.1-SNAPSHOT

--- ols-geocoder-core/src/main/java/ca/bc/gov/ols/geocoder/config/GeocoderConfig.java
+++ ols-geocoder-core/src/main/java/ca/bc/gov/ols/geocoder/config/GeocoderConfig.java
@@ -40,7 +40,7 @@ import ca.bc.gov.ols.geocoder.data.enumTypes.MatchPrecision;
 import ca.bc.gov.ols.util.GeomParseUtil;
 
 public class GeocoderConfig {
-	public static final String VERSION = "4.1.0";
+	public static final String VERSION = "4.1.1-SNAPSHOT";
 	public static final String LOGGER_PREFIX = "BGEO.";
 	public static final PrecisionModel BASE_PRECISION_MODEL = new PrecisionModel(1000);
 	private static final Logger logger = LoggerFactory.getLogger(LOGGER_PREFIX
@@ -49,7 +49,7 @@ public class GeocoderConfig {
 	public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	public static final LocalDate NOT_RETIRED_DATE = LocalDate.parse("9999-12-31", DATE_FORMATTER);
-	
+
 	protected GeocoderConfigurationStore configStore;
 	protected int baseSrsCode = -1;
 	protected Polygon baseSrsBounds;
