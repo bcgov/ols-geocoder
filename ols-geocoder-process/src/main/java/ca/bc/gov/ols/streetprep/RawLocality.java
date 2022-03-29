8483f84642d16accfb7433c55aca54491ff2f526
Chris Hodgson <cmhodgson@gmail.com>
2022-03-29 11:22:59 -0700

    Conversion of streetPrep process to java, increment to v4.3.0

---
+++ ols-geocoder-process/src/main/java/ca/bc/gov/ols/streetprep/RawLocality.java
@@ -0,0 +1,34 @@
+package ca.bc.gov.ols.streetprep;
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
+
+import ca.bc.gov.ols.geocoder.data.enumTypes.LocalityType;
+import gnu.trove.map.TIntObjectMap;
+
+public class RawLocality {
+	public int id;
+	public String name;
+	public String source;
+	public LocalityType type;
+	public int stateProvTerrId;
+	public Integer electoralAreaId;
+	public String descriptor;
+	public Integer containingLocalityId;
+	public Geometry geom;
+	public Point point;
+	
+	public String getFullyQualifiedName(TIntObjectMap<RawLocality> localityMap) {
+		if("BCGNIS".equals(source)) {
+			// we add "near" for IRs and "in" for other gnis localities
+			String descriptor = " in ";
+			if(type.equals(LocalityType.INDIAN_RESERVE)) {
+				descriptor = " near ";
+			}
+			
+			String cName = localityMap.get(containingLocalityId).name;
+			return name + descriptor + cName;
+		}
+		return name;
+	}
+}
---
+++ ols-geocoder-process/src/main/java/ca/bc/gov/ols/streetprep/RawStreetIntersection.java
@@ -0,0 +1,9 @@
+package ca.bc.gov.ols.streetprep;
+
+import org.locationtech.jts.geom.Point;
+
+public class RawStreetIntersection {
+	int id;
+	int degree;
+	Point point;
+}
