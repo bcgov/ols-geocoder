414a74eddced4ed3fdc55f8406ddb82cb2a59a79
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2022-08-08 10:06:25 -0700

    move in memory config logic

--- ols-geocoder-core/src/main/java/ca/bc/gov/ols/geocoder/config/InMemoryGeocoderConfigurationStore.java
+++ ols-geocoder-core/src/main/java/ca/bc/gov/ols/geocoder/config/InMemoryGeocoderConfigurationStore.java
@@ -3,12 +3,47 @@ package ca.bc.gov.ols.geocoder.config;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Stream;
-import ca.bc.gov.ols.config.InMemoryConfigurationStore;
 
-public class InMemoryGeocoderConfigurationStore extends InMemoryConfigurationStore implements GeocoderConfigurationStore {
+import ca.bc.gov.ols.config.ConfigurationParameter;
+import ca.bc.gov.ols.config.ConfigurationStore;
+
+public class InMemoryGeocoderConfigurationStore implements GeocoderConfigurationStore {
 
     public InMemoryGeocoderConfigurationStore(Properties bootstrapConfig) {
         super();
+    }
+
+    @Override
+    public Stream<ConfigurationParameter> getConfigParams() {
+        ConfigurationParameter cp1 = new ConfigurationParameter("BGEO", "dataSource.className", "ca.bc.gov.ols.geocoder.datasources.TestDataSource");
+        ConfigurationParameter cp2 = new ConfigurationParameter("BGEO", "baseSrsCode", "3005");
+        ConfigurationParameter cp3 = new ConfigurationParameter("BGEO", "baseSrsBounds", "200000,300000,1900000,1800000");
+
+        return Stream.of(cp1, cp2, cp3);
+    }
+
+    @Override
+    public void setConfigParam(ConfigurationParameter param) {
+        // not implemented
+    }
+
+    @Override
+    public void removeConfigParam(ConfigurationParameter param) {
+        // not implemented;
+    }
+
+    @Override
+    public void replaceWith(ConfigurationStore configStore) {
+        // not implemented
+    }
+
+    protected void writeConfigParams(List<ConfigurationParameter> configParams) {
+        // not implemented
+    }
+
+    @Override
+    public void close() {
+        // no-op
     }
 
     @Override
