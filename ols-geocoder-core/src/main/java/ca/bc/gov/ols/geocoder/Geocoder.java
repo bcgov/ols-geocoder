8445bccf2682b67ac0f71048ad443d945e5190b6
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2022-08-22 21:41:58 -0700

    try to move files

--- ols-geocoder-core/src/main/java/ca/bc/gov/ols/geocoder/Geocoder.java
+++ ols-geocoder-core/src/main/java/ca/bc/gov/ols/geocoder/Geocoder.java
@@ -103,8 +103,8 @@ public class Geocoder implements IGeocoder {
 	//private DateFormatter dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 	private Lexer lexer;
 	private SiteAddress fallbackSiteAddress;
-	private static String dataDir = "/usr/local/libpostal/";
-//	private static String dataDir = "src/main/resources/libpostal_data/";
+//	private static String dataDir = "/usr/local/libpostal/";
+	private static String dataDir = "src/main/resources/libpostal_data/";
 
 	public Geocoder(GeocoderDataStore datastore) {
 		this.datastore = datastore;
